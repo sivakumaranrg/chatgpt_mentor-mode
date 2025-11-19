f35e285f0e2823ed2ba79f55344de0766207b6f4
EC2 Default User <ec2-user@ip-172-31-78-76.ec2.internal>
2025-11-19 15:15:15 +0000

    updated

--- ec2.sh
+++ ec2.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # 1. Update System
-sudo yum update -y
+sudo yum update -y > /dev/null 2>&1
 
 # 2. Install Git, Docker, and Make
-sudo yum install git docker make -y
+sudo yum install git docker make -y > /dev/null 2>&1
 
 # 3. Configure Docker Permissions for ec2-user
-sudo usermod -aG docker ec2-user
+sudo usermod -aG docker ec2-user > /dev/null 2>&1
 
 # 4. Start Docker Service
-sudo systemctl enable --now docker
+sudo systemctl enable --now docker > /dev/null 2>&1
 
 # 5. Install Kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-sudo rm kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" > /dev/null 2>&1
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl > /dev/null 2>&1
+sudo rm kubectl > /dev/null 2>&1
 
 # 6. Install Minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
-sudo rpm -Uvh minikube-latest.x86_64.rpm
-sudo rm minikube-latest.x86_64.rpm
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm > /dev/null 2>&1
+sudo rpm -Uvh minikube-latest.x86_64.rpm > /dev/null 2>&1
+sudo rm minikube-latest.x86_64.rpm > /dev/null 2>&1
 
 # --- NEW SECTION ---
 
@@ -29,5 +29,5 @@ sudo rm minikube-latest.x86_64.rpm
 cat <<EOF >> /home/ec2-user/.bashrc
 alias k=kubectl
 EOF
-source /home/ec2-user/.bashrc
+echo "source /home/ec2-user/.bashrc" > /dev/null 2>&1
 
